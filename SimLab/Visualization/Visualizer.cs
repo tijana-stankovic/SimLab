502f4e6415bd69c2db9141e9ac30998ca70711dc
Tijana Stankovic <tijana.stankovic@gmail.com>
2026-03-09 23:46:47 +0100

    Added dynamically calculated grid (based on cell positions) and HUD (Heads-Up Display) information

--- SimLab/Visualization/Visualizer.cs
+++ SimLab/Visualization/Visualizer.cs
@@ -1,6 +1,7 @@
 using SimLab.Simulator;
 using System.Numerics;
 using Raylib_cs;
+using RayColor = Raylib_cs.Color;
 
 namespace SimLab.Visualization;
 
@@ -8,53 +9,76 @@ namespace SimLab.Visualization;
 internal class Visualizer {
     private const int ScreenWidth = 1200;
     private const int ScreenHeight = 800;
+    private const float CellSize = 1.0f;
+
     public int Show(FrameBuffer frameBuffer) {
         int currentFrameIndex = frameBuffer.GetStartFrameIndex();
         if (currentFrameIndex < 0) {
             return -1;
         }
-
+        Frame? currentFrame = frameBuffer.GetFrame(currentFrameIndex);
+        if (currentFrame == null) {
+            return -1;
+        }
 
         Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
         Raylib.InitWindow(ScreenWidth, ScreenHeight, "SimLab Visualization");
         Raylib.SetTargetFPS(60);
 
         Camera3D camera = new Camera3D(
-            new Vector3(1, 30, 1), // position
-            new Vector3(0, 0, 0),  // target
-            new Vector3(0, 1, 0),  // up vector
-            45,                    // fov
-            CameraProjection.Perspective
+            new Vector3(10, 10, 10),        // position (camera is located at this point)
+            new Vector3(0, 0, 0),           // target (camera looks at this point)
+            new Vector3(0, 1, 0),           // up vector (used to determine the camera's orientation)
+            45,                             // fov (degrees, defines the field of view)
+            CameraProjection.Perspective    // projection mode (perspective or orthographic)
         );
 
         try {
             while (!Raylib.WindowShouldClose()) {
+                Frame frame = frameBuffer.GetFrame(currentFrameIndex)!;
+
                 // handle left and right arrow keys to navigate through frames
-                if (Raylib.IsKeyPressed(KeyboardKey.Left)) {
+                if (Raylib.IsKeyDown(KeyboardKey.Left)) {
                     currentFrameIndex = Math.Max(currentFrameIndex - 1, 0);
                 }
-                if (Raylib.IsKeyPressed(KeyboardKey.Right)) {
+                if (Raylib.IsKeyDown(KeyboardKey.Right)) {
                     currentFrameIndex = Math.Min(currentFrameIndex + 1, frameBuffer.Count - 1);
                 }                
 
                 Raylib.UpdateCamera(ref camera, CameraMode.Free);
 
                 Raylib.BeginDrawing();
-                Raylib.ClearBackground(Raylib_cs.Color.Red);
+                Raylib.ClearBackground(RayColor.Black);
 
                 Raylib.BeginMode3D(camera);
 
-                Raylib.DrawGrid(20, 1.0f);
+                DrawGrid(frameBuffer, frame);
+                Raylib.DrawLine3D(new Vector3(0, 0, 0), new Vector3(5, 0, 0), RayColor.Red);   // X osa
+                Raylib.DrawLine3D(new Vector3(0, 0, 0), new Vector3(0, 5, 0), RayColor.Green); // Y osa
+                Raylib.DrawLine3D(new Vector3(0, 0, 0), new Vector3(0, 0, 5), RayColor.Blue);  // Z osa
 
                 // draw cells from framebuffer
-                Frame? frame = frameBuffer.GetFrame(currentFrameIndex);
-                if (frame != null) {
-                    foreach (Position pos in frame.Cells) {
-                        Raylib.DrawCube(new Vector3(pos.X, pos.Z, pos.Y), 0.5f, 0.5f, 0.5f, Raylib_cs.Color.Yellow);
-                    }
+                foreach (Position pos in frame.Cells) {
+                    // if (frameBuffer.WorldSpace == 2) {
+                    //     Raylib.DrawCube(new Vector3(pos.X, pos.Z, pos.Y), CellSize, CellSize, CellSize, RayColor.Yellow);
+                    // } else if (frameBuffer.WorldSpace == 3) {
+                    //     Raylib.DrawCube(new Vector3(pos.X, pos.Y, pos.Z), CellSize, CellSize, CellSize, RayColor.Yellow);
+                    // }
+                    Vector3 cellPosition = AdjustedCellPosition(frameBuffer.WorldSpace, pos);
+                    Raylib.DrawCube(cellPosition, CellSize, CellSize, CellSize, RayColor.Lime);
+                    Raylib.DrawCubeWires(cellPosition, CellSize, CellSize, CellSize, new RayColor(20, 80, 20, 255));
                 }
 
                 Raylib.EndMode3D();
+
+                // HUD information
+                Raylib.DrawText($"Frame: {currentFrameIndex + 1}/{frameBuffer.Count}", 10, 10, 20, RayColor.White);
+                if (frame != null)
+                    Raylib.DrawText($"Cells: {frame.Cells.Count}", 10, 40, 20, RayColor.White);
+                Raylib.DrawText("[LeftMouseButton]=orbit  [Wheel]=zoom  WASD/QE=move", 10, 70, 20, RayColor.LightGray);
+                Raylib.DrawText("LEFT/RIGHT = frame navigation", 10, 100, 20, RayColor.LightGray);
+                Raylib.DrawText("ESC = close visualization", 10, 130, 20, RayColor.LightGray);
+
                 Raylib.EndDrawing();
             }
         } finally {
@@ -63,5 +87,74 @@ internal class Visualizer {
 
         frameBuffer.SetLastViewedFrameIndex(currentFrameIndex);
         return currentFrameIndex;
+    }
+
+    // dinamically calculates grid bounds based on world dimensions and cell positions in the frame, 
+    // then draws the grid lines
+    private static void DrawGrid(FrameBuffer frameBuffer, Frame frame) {
+        // first, calculates grid bounds based on frame buffer world dimensions and cell positions
+
+        // when any world dimension are not specified, use default value of 10
+        int worldWidth = frameBuffer.WorldDimensions.Length > 0 ? frameBuffer.WorldDimensions[0] : 10;
+        int worldDepth;
+        if (frameBuffer.WorldSpace == 2) {
+            // in 2D world, we use the second dimension for worldDepth
+            worldDepth = frameBuffer.WorldDimensions.Length > 1 ? frameBuffer.WorldDimensions[1] : 10;
+        } else {
+            // in 3D world, we use the third dimension for worldDepth
+            worldDepth = frameBuffer.WorldDimensions.Length > 2 ? frameBuffer.WorldDimensions[2] : 10;
+        }
+
+        // initial values for grid bounds
+        // then it will be adjusted based on cell positions in the frame
+        int minX = 0;
+        int minZ = 0;
+        int maxX = Math.Max(worldWidth, 10); // 10 is the minimum grid size
+        int maxZ = Math.Max(worldDepth, 10); // 10 is the minimum grid size
+
+        foreach (Position pos in frame.Cells) {     
+            minX = Math.Min(minX, pos.X);
+            maxX = Math.Max(maxX, pos.X + 1);
+
+            if (frameBuffer.WorldSpace == 2) {
+                // in 2D world, we use the second dimension 
+                minZ = Math.Min(minZ, pos.Y);
+                maxZ = Math.Max(maxZ, pos.Y + 1);               
+            } else {
+                // in 3D world, we use the third dimension 
+                minZ = Math.Min(minZ, pos.Z);
+                maxZ = Math.Max(maxZ, pos.Z + 1);                               
+            }
+        }
+
+        // draw grid lines
+        RayColor lineColor = RayColor.DarkGray; // grid line color
+
+        for (int x = minX; x <= maxX; x++) {
+            Raylib.DrawLine3D(
+                new Vector3(x, 0f, minZ),
+                new Vector3(x, 0f, maxZ),
+                lineColor);
+        }
+
+        for (int z = minZ; z <= maxZ; z++) {
+            Raylib.DrawLine3D(
+                new Vector3(minX, 0f, z),
+                new Vector3(maxX, 0f, z),
+                lineColor);
+        }
+
+    }
+
+    // Adjusts the position of a cell based on the world space (2D or 3D).
+    // In 2D world, the Y coordinate is used for depth (Z axis in 3D), 
+    // while in 3D world, the Z coordinate is used for depth.
+    // Also, the position is adjusted to be aligned with the grid lines.
+    private static Vector3 AdjustedCellPosition(int worldSpace, Position position) {
+        if (worldSpace == 2) {
+            return new Vector3(position.X + CellSize/2, CellSize/2, position.Y + CellSize/2);
+        }
+
+        return new Vector3(position.X + CellSize/2, position.Y + CellSize/2, position.Z + CellSize/2);
     }
 }
