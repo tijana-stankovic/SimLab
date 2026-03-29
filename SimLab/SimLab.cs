c15e2261280bd2bd44982c29940d1305738994ba
Tijana Stankovic <tijana.stankovic@gmail.com>
2026-03-29 04:18:54 +0200

    Added initial PostgreSQL connectivity

--- SimLab/SimLab.cs
+++ SimLab/SimLab.cs
@@ -1,4 +1,5 @@
 ﻿using SimLab.Cmd;
+using Npgsql;
 
 namespace SimLab;
 
@@ -16,7 +17,43 @@ internal class SimLab {
     /// 'configuration-file-name', which is the name of the SimLab configuration file.
     /// </summary>
     /// <param name="args">The command-line arguments passed to the program.</param>
-    public static void Main(string[] args) {
+
+
+    // public static void Main(string[] args) {
+    public static async Task Main(string[] args) {
+        static string ReadPassword() {
+            var password = "";
+            ConsoleKey key;
+
+            do {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+
+                if (key == ConsoleKey.Backspace && password.Length > 0) {
+                    password = password[..^1];
+                } else if (!char.IsControl(keyInfo.KeyChar)) {
+                    password += keyInfo.KeyChar;
+                }
+            }
+            while (key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+            return password;
+        }
+
+        Console.Write("Enter password for user 'simlab': ");
+        string password = ReadPassword();
+        var connectionString = $"Host=localhost;Port=5432;Database=simlab;Username=simlab;Password={password}";
+        await using var dataSource = NpgsqlDataSource.Create(connectionString);
+        await using var conn = await dataSource.OpenConnectionAsync();
+        await using var cmd = new NpgsqlCommand("SELECT current_user, current_database()", conn);
+        await using var reader = await cmd.ExecuteReaderAsync();
+        if (await reader.ReadAsync()) {
+            Console.WriteLine($"User: {reader.GetString(0)}");
+            Console.WriteLine($"Database: {reader.GetString(1)}");
+        }
+
+
         if (args.Length <= 1) {
             Controller controller = new(args);
             controller.Run();
